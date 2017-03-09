bcc557d16ed69f9eb41219ad7d261390ef01e180
mmuchai <muchaimercyw@gmail.com>
2017-03-09 13:34:54 +0300

    Checks that the parameters are only type(list)

--- missing_numbers.py
+++ missing_numbers.py
@@ -1,6 +1,6 @@
 def find_missing(array_one, array_two):
 	the_difference = set(array_one) ^ set(array_two)
-	if (array_one and array_two) == [ ]:
+	if (isinstance (array_one, list) and isinstance(array_two, list) == [ ]:
 		return 0
 	elif array_one == array_two:
 		return 0
