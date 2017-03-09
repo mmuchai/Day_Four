ae2eecc0a2d09885fac00b6ce5327f84a4abe0e8
mmuchai <muchaimercyw@gmail.com>
2017-03-09 13:33:16 +0300

    All tests running

--- missing_numbers.py
+++ missing_numbers.py
@@ -2,7 +2,7 @@ def find_missing(array_one, array_two):
 	the_difference = set(array_one) ^ set(array_two)
 	if (array_one and array_two) == [ ]:
 		return 0
-	elif set(array_one) == set(array_two):
+	elif array_one == array_two:
 		return 0
 	else:
-		return the_difference
+		return (list(the_difference) [0])
